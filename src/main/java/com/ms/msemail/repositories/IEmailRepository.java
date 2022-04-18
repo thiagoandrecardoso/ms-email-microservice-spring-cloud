42cb6594997a0f454b797e3a67e9b1e36aa06116
Thiago <thiagoandrecardoso@icloud.com>
2022-04-18 14:50:06 -0300

    Alterado tipo do ID para se comunicar melhor com o AMQP

--- src/main/java/com/ms/msemail/repositories/IEmailRepository.java
+++ src/main/java/com/ms/msemail/repositories/IEmailRepository.java
@@ -3,5 +3,7 @@ package com.ms.msemail.repositories;
 import com.ms.msemail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IEmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface IEmailRepository extends JpaRepository<EmailModel, UUID> {
 }
